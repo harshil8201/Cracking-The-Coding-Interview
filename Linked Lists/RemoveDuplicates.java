88ac3a25e34472107f610708f5e6c6571ea53bec
nirali2831 <nirali28200@gmail.com>
2024-08-13 10:49:11 -0400

    commit

--- Linked Lists/RemoveDuplicates.java
+++ Linked Lists/RemoveDuplicates.java
@@ -19,27 +19,50 @@ public class RemoveDuplicates {
         head.next.next = removeDuplicates.new Node(12);
         head.next.next.next = removeDuplicates.new Node(11);
         head.next.next.next.next = removeDuplicates.new Node(13);
+        head.next.next.next.next.next = removeDuplicates.new Node(12);
+        head.next.next.next.next.next.next = removeDuplicates.new Node(14);
 
         removeDuplicates.deleteDuplicates(head);
         removeDuplicates.printList(head);
     }
     
-    // Solution 1:
+    // // Solution 1:
+    // void deleteDuplicates(Node head) {
+    //     HashSet<Integer> nodeSet = new HashSet<Integer>();
+
+    //     // to keep data of prevoius node,
+    //     // when we found duplicates, we can attech prevoius to next node.
+    //     Node previous = null;
+
+    //     while (head != null) {
+    //         if (nodeSet.contains(head.data)) {
+    //             previous.next = head.next;
+    //         } else {
+    //             nodeSet.add(head.data);
+    //             previous = head;
+    //         }
+    //         head = head.next;
+    //     }
+    // }
+
+    // Solution 2: without buffer (without Storing nodes)
+    // method: two pointer (current-faster)
+    // tc: 0(n^2) - sc: o(1)
+
     void deleteDuplicates(Node head) {
-        HashSet<Integer> nodeSet = new HashSet<Integer>();
+        Node current = head;
 
-        // to keep data of prevoius node,
-        // when we found duplicates, we can attech prevoius to next node.
-        Node previous = null;
-
-        while (head != null) {
-            if (nodeSet.contains(head.data)) {
-                previous.next = head.next;
-            } else {
-                nodeSet.add(head.data);
-                previous = head;
+        while (current != null) {
+            // remove all future node that have same values
+            Node runner = current;
+            while (runner.next != null) {
+                if (runner.next.data == current.data) {
+                    runner.next = runner.next.next;
+                } else {
+                    runner = runner.next;
+                }
             }
-            head = head.next;
+            current = current.next;
         }
     }
 
